287bc05716d57b8db325754bb5b47ad68261196f
ItsMeAaryan <98041560+ItsMeAaryan@users.noreply.github.com>
2026-05-03 16:19:14 +0530

    Update university.ts

--- types/university.ts
+++ types/university.ts
@@ -48,3 +48,64 @@ export interface ExtendedUniversityProfile {
 
 export interface UniversityDetails
   extends ExtendedUniversityProfile {}
+
+export interface UniversityFilters {
+  search?: string
+
+  city?: string
+
+  state?: string
+
+  program?: string
+
+  rating?: number
+}export interface ExtendedUniversityProfile {
+  uid?: string
+
+  name?: string
+
+  email?: string
+
+  phone?: string
+
+  website?: string
+
+  city?: string
+
+  state?: string
+
+  country?: string
+
+  logoUrl?: string
+
+  bannerUrl?: string
+
+  description?: string
+
+  establishedYear?: string
+
+  universityType?: string
+
+  accreditation?: string
+
+  rating?: number
+
+  applicationDeadline?: string
+
+  programs?: string[]
+
+  facilities?: string[]
+
+  documentsRequired?: string[]
+
+  admissionProcess?: string
+
+  contactPerson?: string
+
+  createdAt?: any
+
+  updatedAt?: any
+}
+
+export interface UniversityDetails
+  extends ExtendedUniversityProfile {}
